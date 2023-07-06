b4aa9573ae42429562f74f02b0179ac3060141cb
Fabian Alfaro <Ranshesi7@gmail.com>
2023-07-06 14:05:34 -0500

    ARRAYS EN JAVASCRIPTO 1

---
+++ scripto.js
@@ -0,0 +1,10 @@
+const frutas=[];
+/* Agregar un dato al arreglo*/
+frutas.push("pera")
+/* Agregar un dato al arreglo en el indice 1*/
+frutas.push("tetas")
+/* Eliminar último elemento al arreglo*/
+/* El elemento sacado sera tetas*/
+frutas.pop()
+/* IMPRESION DEL ARRAY,SOLO ESTA EL VALOR PERA*/
+console.log(frutas);
